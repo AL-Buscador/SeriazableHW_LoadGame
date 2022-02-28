5315b8c0a5c8eb88c94243886b55547be878028b
AL-Buscador <donpost@internet.ru>
2022-02-28 13:04:35 +0300

    add unzip cycle

--- src/Home/Main.java
+++ src/Home/Main.java
@@ -1,5 +1,6 @@
 package Home;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.ObjectInputStream;
@@ -9,24 +10,33 @@ import java.util.zip.ZipInputStream;
 public class Main {
 
     public static void main(String[] args) {
+        String path = "D:\\Мои документы\\Java Files\\JavaHW\\Games\\savegames\\";
+
         try (ZipInputStream zin = new ZipInputStream(
-                new FileInputStream("D:\\Мои документы\\Java Files\\JavaHW\\Games\\savegames\\savedFIles.zip"))) {
+                new FileInputStream(path + "savedFIles.zip"))) {
             ZipEntry entry;
             String name;
-            entry = zin.getNextEntry();
-            name = entry.getName();
-            FileOutputStream fout = new FileOutputStream(
-                    "D:\\Мои документы\\Java Files\\JavaHW\\Games\\savegames\\" + name);
-            for (int c = zin.read(); c != -1; c = zin.read()) {
-                fout.write(c);
+
+            while ((entry = zin.getNextEntry()) != null) {
+                name = entry.getName();
+                FileOutputStream fout = new FileOutputStream(
+                        path + name);
+                for (int c = zin.read(); c != -1; c = zin.read()) {
+                    fout.write(c);
+                }
+                fout.flush();
+                zin.closeEntry();
+                fout.close();
             }
-            fout.flush();
-            zin.closeEntry();
-            fout.close();
 
             GameProgress gameProgress = null;
+            File file = new File(path);
+            File[] unpackedList = file.listFiles();
+            String nameFile = null;
+            if (unpackedList[0].isFile())
+                nameFile = unpackedList[0].getName();
             try (FileInputStream fis = new FileInputStream(
-                    "D:\\Мои документы\\Java Files\\JavaHW\\Games\\savegames\\" + name);
+                    path + nameFile);
                  ObjectInputStream ois = new ObjectInputStream(fis)) {
                 gameProgress = (GameProgress) ois.readObject();
             } catch (Exception ex) {
